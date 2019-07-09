41de08126b5e00d8d9494ae05961db92a30d8941
Jerry Wang <jerryappleid761208@gmail.com>
2019-07-09 16:07:16 -0700

    link list

---
+++ client/components/link_list.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { createContainer } from 'meteor/react-meteor-data'
+import { Meteor } from 'meteor/meteor';
+import { Links } from '../../imports/collections/links';
+
+class LinkList extends Component {
+  renderRows() {
+    return this.props.links.map((link) => {
+      const { url, clicks, token } = link;
+      const shortLink = `http//localhost:3000/${token}`;
+      return (
+        <tr key={link._id}>
+          <td>{url}</td>
+          <td><a href={shortLink}>{shortLink}</a></td>
+          <td>{clicks}</td>
+        </tr>
+      )
+    })
+  }
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>URL</th>
+            <th>Address</th>
+            <th>Clicks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderRows(LinkList)}
+        </tbody>
+      </table>
+    )        
+  }
+}
+
+
+export default createContainer(() => {
+  Meteor.subscribe('links');
+  return { links: Links.find({}).fetch() };
+}, LinkList);
--- imports/collections/links.js
+++ imports/collections/links.js
@@ -4,7 +4,12 @@ import { check, Match } from 'meteor/check';
 
 Meteor.methods({
   'links.insert': function(url) {    
+    // validate
     check(url, Match.Where(url => validUrl.isUri(url)));
+
+    // We're ready to save the url
+    const token = Math.random().toString(36).slice(-5);
+    Links.insert({ url, token, clicks: 0});
   }
 })
 
