d1d7c7502c9058a9e14a22b520e77f0acbb7ce71
Jerry Wang <jerryappleid761208@gmail.com>
2019-07-09 15:15:53 -0700

    form submit

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react'
 import { Meteor } from 'meteor/meteor';
 
 class LinkCreate extends Component {
+  state = {
+    error: ''
+  }
+
 
   handleSubmit = (e) => {
     e.preventDefault(); // prevent reload page of submit event
-    Meteor.call('links.insert', this.refs.link.value);
+    Meteor.call('links.insert', this.refs.link.value, (error) => {
+      if (error) {
+        this.setState({ error: 'Enter a valid URL'});
+      } else {
+        this.setState({ error: '' });
+        this.refs.link.value = '';
+      }
+    });
   }
 
   render() {
@@ -15,6 +26,7 @@ class LinkCreate extends Component {
           <label>Link to shorten</label>
           <input ref="link" className="form-control" />
         </div>
+        <div className="text-danger">{this.state.error}</div>
         <button className="btn btn-primary">Shorten!</button>
       </form>
     )
